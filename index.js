d52883f19b99ebdb9c7d49c8e625139300890b6d
Ali Qashlan <aliqashlan99@gmail.com>
2025-04-10 00:24:42 +0200

    Added morgan middleware for logging POST request data

--- index.js
+++ index.js
@@ -1,15 +1,103 @@
 const express = require("express");
 
+const morgan = require("morgan");
+
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello, Phonebook Backend!");
+app.use(express.json());
+
+morgan.token("body", (req) => JSON.stringify(req.body));
+
+app.use(
+  morgan(
+    ":method :url :status :response-time ms - :res[content-length] :body"
+  )
+);
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+];
+
+
+
+app.get("/api/persons", (req, res) => {
+    res.json(persons);
 });
 
+app.get("/info", (req, res) => {
+  const date = new Date();
+  const count = persons.length;
 
+  res.send(`
+    <div>
+    <p>Phonebook has info for ${count} people </p>
+    <p>${date}</p>
+    </div>
+    `);
+});
 
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find(person => person.id === id);
 
+  if (person) {
+    res.json(person);
+  }
+  else {
+    res.status(404).send({ error: "Person not found" });
+  }
+});
 
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter(person => person.id !== id);
+
+  res.status(204).end();
+
+});
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+
+  if (!body.name || !body.number) {
+  return  res.status(400).json({ error: "name and number are required"})
+  };
+
+  const existingPerson = persons.find(person => person.name === body.name);
+
+if (existingPerson) {
+  return res.status(400).json({ error: "name must be unique"});
+}
+const id = Math.floor(Math.random() * 100000);
+
+const newPerson = {
+  id,
+  name: body.name,
+  number: body.number
+};
+persons.push(newPerson);
+
+res.status(201).json(newPerson);
+});
 
 const PORT = 3001;
 
