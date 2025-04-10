e03b1f9ecec20ce724ac3b26e9fd94fc1bf40108
Ali Qashlan <aliqashlan99@gmail.com>
2025-04-10 21:15:23 +0200

    Add dynamic port for deployment

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ persons.push(newPerson);
 res.status(201).json(newPerson);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
